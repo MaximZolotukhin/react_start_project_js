575dfab6c603365c58bdd0928ef64dd01b4f9391
VelesDragon <ScorpionEvil777@gmai.com>
2026-03-05 11:12:31 +0300

    Добавил компонент Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Заголовок</h1>
+      <p>Текст</p>
+      <Button />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css';
+
+function Button() {
+  return (
+    <>
+      <button>Сохранить</button>
+    </>
+  );
+}
+
+export default Button;
