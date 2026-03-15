a3428a4c592a16a2eba858da9ac9d105661f3f48
VelesDragon <ScorpionEvil777@gmai.com>
2026-03-15 23:12:28 +0300

    Добавили картнки для конпки новая заметка

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css';
 import Button from './components/Button/Button';
 import JournalItem from './components/JournalItem/JournalItem';
 import CardButton from './components/CardButton/CardButton';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
+import Header from './components/Header/Header';
+import Body from './layouts/Body/Body';
+import JournalList from './components/JournalList/JournalList';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 
 function App() {
   const data = [
@@ -23,33 +28,36 @@ function App() {
   ];
 
   return (
-    <>
-      <h1>Заголовок</h1>
-      <p>Текст</p>
-      <Button />
-      <CardButton>Новое событие</CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[0].title}
-          text={data[0].text}
-          date={data[0].date}
-        />
-      </CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[1].title}
-          text={data[1].text}
-          date={data[1].date}
-        />
-      </CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[2].title}
-          text={data[2].text}
-          date={data[2].date}
-        />
-      </CardButton>
-    </>
+    <div className="app">
+      <LeftPanel>
+        <Header />
+        <JournalAddButton />
+        <JournalList>
+          <CardButton>
+            <JournalItem
+              title={data[0].title}
+              text={data[0].text}
+              date={data[0].date}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+              title={data[1].title}
+              text={data[1].text}
+              date={data[1].date}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+              title={data[2].title}
+              text={data[2].text}
+              date={data[2].date}
+            />
+          </CardButton>
+        </JournalList>
+      </LeftPanel>
+      <Body>Body</Body>
+    </div>
   );
 }
 
